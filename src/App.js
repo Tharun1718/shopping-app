e474fc4646a5b58001b2d606d9136b8acd1c778a
Tharun1718 <tharunmano1170@gmail.com>
2022-10-04 16:42:33 +0530

    updated add to cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const items = [
+    {
+      name: "Samsung Galaxy",
+      price: "$200",
+      rating: "⭐⭐⭐⭐",
+      image: "https://m.media-amazon.com/images/I/71qZERyxy6L.jpg"
+    },
+    {
+      name: "Samsung Galaxy",
+      price: "$200",
+      rating: "⭐⭐⭐⭐",
+      image: "https://m.media-amazon.com/images/I/71qZERyxy6L.jpg"
+    },
+    {
+      name: "Samsung Galaxy",
+      price: "$200",
+      rating: "⭐⭐⭐⭐",
+      image: "https://m.media-amazon.com/images/I/71qZERyxy6L.jpg"
+    },
+    {
+      name: "Samsung Galaxy",
+      price: "$200",
+      rating: "⭐⭐⭐⭐",
+      image: "https://m.media-amazon.com/images/I/71qZERyxy6L.jpg"
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardList items={items} />
     </div>
   );
 }
+
+function CardList({items}){
+  return(
+    <div className='card-list-container'>
+      {items.map((item) => (
+        <Card item={item} />
+      ))}
+    </div>
+  )
+}
+
+function Card({item}){
+  // const item = {
+  //   name: "Samsung Galaxy",
+  //   price: "$200",
+  //   rating: "⭐⭐⭐⭐",
+  //   image: "https://m.media-amazon.com/images/I/71qZERyxy6L.jpg"
+  // }
+  const [cartList, setCartList] = useState([]);
+
+  return(
+    <div className="card-container">
+      <img src={item.image} alt={item.name} />
+      <h2>{item.name}</h2>
+      <p>{item.rating}</p>
+      <p>{item.price}</p>
+      <button
+      onClick={()=>{
+        const newItem ={
+          name: item.name,
+          price: item.price,
+          rating: item.rating,
+          image: item.image
+        };
+        console.log(newItem);
+        setCartList([...cartList,newItem])
+        console.log(cartList); // item has been added to the cartList array
+      }}
+      >Add to Cart</button>
+    </div>
+  )
+}
+
+
 
 export default App;
