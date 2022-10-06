7e6287bf7b6298d31ce0b9b3722b3c96c5acd9e7
Tharun1718 <tharunmano1170@gmail.com>
2022-10-06 22:17:40 +0530

    adding routes- cart page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import {useState} from 'react';
-
+import { Routes, Route, Link, Navigate, useNavigate} from 'react-router-dom';
 function App() {
   const items = [
     {
@@ -29,18 +29,41 @@ function App() {
       image: "https://m.media-amazon.com/images/I/71qZERyxy6L.jpg"
     }
   ]
+  const navigate = useNavigate();
   return (
     <div className="App">
+      <button onClick={()=>navigate("/home")}>Home</button>
+      <button onClick={()=>navigate("/cartItems")}>Cart</button>
       <CardList items={items} />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/cartItems" element={<CartItems />} />
+      </Routes>
     </div>
   );
+}
+
+function Home(){
+  return(
+    <div>
+      <h1>Welcome to my home page</h1>
+    </div>
+  )
+}
+
+function CartItems(){
+  return(
+    <div>
+      <h2>I am an cart page</h2>
+    </div>
+  )
 }
 
 function CardList({items}){
   return(
     <div className='card-list-container'>
-      {items.map((item) => (
-        <Card item={item} />
+      {items.map((item,index) => (
+        <Card key={index} item={item} />
       ))}
     </div>
   )
@@ -54,14 +77,15 @@ function Card({item}){
   //   image: "https://m.media-amazon.com/images/I/71qZERyxy6L.jpg"
   // }
   const [cartList, setCartList] = useState([]);
-
+  const [show, setShow] = useState(true);
+  const navigate = useNavigate();
   return(
     <div className="card-container">
       <img src={item.image} alt={item.name} />
       <h2>{item.name}</h2>
       <p>{item.rating}</p>
       <p>{item.price}</p>
-      <button
+      {show ? <button
       onClick={()=>{
         const newItem ={
           name: item.name,
@@ -70,13 +94,17 @@ function Card({item}){
           image: item.image
         };
         console.log(newItem);
-        setCartList([...cartList,newItem])
+        setCartList([...cartList,newItem]);
         console.log(cartList); // item has been added to the cartList array
+        setShow(false)
+        navigate("/cartItems")
       }}
-      >Add to Cart</button>
+      >Add to Cart</button> : null } 
     </div>
   )
 }
+
+
 
 
 
